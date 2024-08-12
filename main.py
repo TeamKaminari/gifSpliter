c2079d8ccc74e171c409ec4e7ae1bbf6e9d6071a
deraze <136134371+Empty1791@users.noreply.github.com>
2024-08-12 14:49:48 +0800

    fix an error

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from tkinter import messagebox
 import split
-directory = input("Enter Image Directory: ")
+directory = input("Enter Image path: ")
 folder = input("Enter Folder Name: ")
 split.split_terminal(directory, folder)
 messagebox.showinfo("Successful", "Image Split Complete")
